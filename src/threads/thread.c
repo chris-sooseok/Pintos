7b7078fe2476a87f963af69b357839e23e031816
nwlasher <nwlasher@buffalo.edu>
2026-02-24 16:35:05 -0500

    Noted sorting entry in thread_unblock

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -252,6 +252,9 @@ thread_unblock (struct thread *t)
 
   old_level = intr_disable ();
   ASSERT (t->status == THREAD_BLOCKED);
+
+  // Sort here! whether on insert or after insert
+  // Make sure not to forget that a highest prio thread preempts the current one with yeilds?
   list_push_back (&ready_list, &t->elem);
   t->status = THREAD_READY;
   intr_set_level (old_level);
